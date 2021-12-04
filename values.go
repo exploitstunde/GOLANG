7dddf115ae4cbedce345d026d738e3633174fc01
exploitstunde <tunde.akindele@ymail.com>
2021-12-04 11:33:12 +0100

    add

--- values.go
+++ values.go
@@ -3,25 +3,25 @@ package main
 import "fmt"
 
 func main(){
-	strings val
+	//strings val
     fmt.Println("123 + 123") // 123 + 123
 	
 	fmt.Println("hello","1","Ahmed") // hello 1 Ahmed 
     
-	int 
+	//int 
 	fmt.Println(1)
     fmt.Println(1, 2)
 	fmt.Println(4 / 2) // 2
 	fmt.Println(5 / 1) // 5
 
-    string & int
+    //string & int
 
 	fmt.Println("hello", 2)
 	fmt.Println("hello", 2, 3)
 	fmt.Println(2 , "hello", 3)
     
 
-	float numbers
+	//float numbers
 
 	fmt.Println("this is float number", 7.0/2.0) // 3.5
 	fmt.Println("this is NOT Float nuber", 7/2) // 3
@@ -29,7 +29,7 @@ func main(){
     fmt.Println("this. is also f n", 7.0/2)
     fmt.Println("this. is also f n", 7/2.0)
 
-	bool
+	//bool
 	fmt.Println("this. is bool", true)
 	fmt.Println("this is bool", false)
 
