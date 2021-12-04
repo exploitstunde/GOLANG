f63bfa7e2bef436d97e403120c8a2d316addb369
exploitstunde <tunde.akindele@ymail.com>
2021-12-04 11:31:45 +0100

    add

--- if-else.go
+++ if-else.go
@@ -4,69 +4,66 @@ import "fmt"
 
 func main(){
 	
-
-
-
-	if name := "sara"
-    name = "ahmed"
-	if name == "ahmed" {
+	// if name := "sara"
+    // name = "ahmed"
+	// if name == "ahmed" {
 		
-		fmt.Println("hey", name)
-	}
+	// 	fmt.Println("hey", name)
+	// }
 
-	fmt.Println("hello")
+	// fmt.Println("hello")
     
-	else 
+	// else 
 
-    name := "sara"
-    name = "ahmed"
-	if name == "ahmed" {
-		//
-		fmt.Println("hey", name)
-	} else {
-		fmt.Println("not auth", name )
-	}
+    // name := "sara"
+    // name = "ahmed"
+	// if name == "ahmed" {
+	
+	// 	fmt.Println("hey", name)
+	// } else {
+	// 	fmt.Println("not auth", name )
+	// }
 
-     else if 
-    playerName :="jack"
-    playerName = "x"
-	if playerName == "jack" {
-		fmt.Println("hi")
-	} else if playerName == "jack" {
-		fmt.Println("hi jack")
-	} else if playerName == "ahmed" {
-      fmt.Println("hay", playerName)
-	} else {
-		fmt.Println("who are you !")
-	}
+    // else if 
+    // playerName :="jack"
+    // playerName = "x"
+	// if playerName == "jack" {
+	// 	fmt.Println("hi")
+	// } else if playerName == "jack" {
+	// 	fmt.Println("hi jack")
+	// } else if playerName == "ahmed" {
+    //   fmt.Println("hay", playerName)
+	// } else {
+	// 	fmt.Println("who are you !")
+	// }
 
-	integer
+	//integer
 
-	age := 21
-	if age == 21 {
-		fmt.Println("age is okay", age)
-	}
+	// age := 21
+	// if age == 21 {
+	// 	fmt.Println("age is okay", age)
+	// }
 	 
-	age := 18
-	if age >= 18 {
-		fmt.Println("age is okay", age)
-	} else {
-		fmt.Println("age is not okay", age)
-	}
+	// age := 18
+	// if age >= 18 {
+	// 	fmt.Println("age is okay", age)
+	// } else {
+	// 	fmt.Println("age is not okay", age)
+	// }
 
-	&& 
-	playerName :="hady"
-    age := 21
+	// && AND
+	// playerName :="hady"
+    // age := 21
     
-	//playerName = "ahmed"
+	// playerName = "ahmed"
 
-	if age >= 18 && playerName == "ahmed"   {
-		fmt.Println("you can drive this car")
-	} else if age >= 18 && playerName == "hady" {
-         fmt.Println("you need permission.")
-	} else {
-		fmt.Println("you can not drive this car")
-	}
+	// if age >= 18 && playerName == "ahmed"   {
+	// 	fmt.Println("you can drive this car")
+	// } else if age >= 18 && playerName == "hady" {
+    //      fmt.Println("you need permission.")
+	// } else {
+	// 	fmt.Println("you can not drive this car")
+	// }
 
 	/*
 		Truth table
@@ -77,25 +74,25 @@ func main(){
 	*/
 
 
-	con inside con
+	//con inside con
 
-	playerName := "amy"
-	age := 17
+	// playerName := "amy"
+	// age := 17
 
-	if age >= 18 {
-       if playerName == "ahemd" {
-		   fmt.Println("you can drive this car")
-	   } else if playerName == "hady" {
-		   fmt.Println("you need permission.")
-	   } else {
-		   fmt.Println("who are you ")
-	   }
-	} else {
-		fmt.Println("you can not drive this car")
-	}
+	// if age >= 18 {
+    //    if playerName == "ahemd" {
+	// 	   fmt.Println("you can drive this car")
+	//    } else if playerName == "hady" {
+	// 	   fmt.Println("you need permission.")
+	//    } else {
+	// 	   fmt.Println("who are you ")
+	//    }
+	// } else {
+	// 	fmt.Println("you can not drive this car")
+	// }
 
 
-	 || 
+	 //|| OR
 
 	playerName := "sara"
 	age := 16
@@ -114,12 +111,6 @@ func main(){
 		false || true = true
 		false || false = false
 	*/
-
-    
-
-
-
-
 
 
 
