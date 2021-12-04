3056eda22349d6b6849f70ec2b55fe433f8021ec
exploitstunde <tunde.akindele@ymail.com>
2021-12-04 08:44:28 +0100

    add

--- if-else.go
+++ if-else.go
@@ -3,21 +3,14 @@ package main
 import "fmt"
 
 func main(){
-
-
-
-
-	
-
 	
 
 
 
-	if con
-    name := "sara"
+	if name := "sara"
     name = "ahmed"
 	if name == "ahmed" {
-		//
+		
 		fmt.Println("hey", name)
 	}
 
@@ -34,46 +27,46 @@ func main(){
 		fmt.Println("not auth", name )
 	}
 
-    //  else if 
-    // playerName :="jack"
-    // playerName = "x"
-	// if playerName == "jack" {
-	// 	fmt.Println("hi")
-	// } else if playerName == "jack" {
-	// 	fmt.Println("hi jack")
-	// } else if playerName == "ahmed" {
-    //   fmt.Println("hay", playerName)
-	// } else {
-	// 	fmt.Println("who are you !")
-	// }
+     else if 
+    playerName :="jack"
+    playerName = "x"
+	if playerName == "jack" {
+		fmt.Println("hi")
+	} else if playerName == "jack" {
+		fmt.Println("hi jack")
+	} else if playerName == "ahmed" {
+      fmt.Println("hay", playerName)
+	} else {
+		fmt.Println("who are you !")
+	}
 
-	// int
+	integer
 
-	// age := 21
-	// if age == 21 {
-	// 	fmt.Println("age is okay", age)
-	// }
+	age := 21
+	if age == 21 {
+		fmt.Println("age is okay", age)
+	}
 	 
-	// age := 18
-	// if age >= 18 {
-	// 	fmt.Println("age is okay", age)
-	// } else {
-	// 	fmt.Println("age is not okay", age)
-	// }
+	age := 18
+	if age >= 18 {
+		fmt.Println("age is okay", age)
+	} else {
+		fmt.Println("age is not okay", age)
+	}
 
-	// && 
-	// playerName :="hady"
-    // age := 21
+	&& 
+	playerName :="hady"
+    age := 21
     
-	// //playerName = "ahmed"
+	//playerName = "ahmed"
 
-	// if age >= 18 && playerName == "ahmed"   {
-	// 	fmt.Println("you can drive this car")
-	// } else if age >= 18 && playerName == "hady" {
-    //      fmt.Println("you need permission.")
-	// } else {
-	// 	fmt.Println("you can not drive this car")
-	// }
+	if age >= 18 && playerName == "ahmed"   {
+		fmt.Println("you can drive this car")
+	} else if age >= 18 && playerName == "hady" {
+         fmt.Println("you need permission.")
+	} else {
+		fmt.Println("you can not drive this car")
+	}
 
 	/*
 		Truth table
@@ -84,10 +77,10 @@ func main(){
 	*/
 
 
-	// con inside con
+	con inside con
 
-	// playerName := "amy"
-	// age := 17
+	playerName := "amy"
+	age := 17
 
 	if age >= 18 {
        if playerName == "ahemd" {
